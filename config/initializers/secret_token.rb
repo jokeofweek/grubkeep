9caf32fee06322ff340cf69d3ccd2a148305dd82
jokeofweek <dominiccharleyroy@gmail.com>
2013-12-27 22:22:34 -0500

    Rails init.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Grubkeep::Application.config.secret_key_base = '8755f9a899b4b7d7e35f91f26c3facda526db280f92353323d6d4c515cfeb33bf800c41bda6d122f767dcbd0b5a80aafbffd5cfd38d06b4b69a75b1019736c31'
